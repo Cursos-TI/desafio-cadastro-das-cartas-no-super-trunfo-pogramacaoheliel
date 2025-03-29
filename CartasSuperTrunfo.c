ffaf5ac21b96c7b61233f9a9cfb77c2f35abd219
pogramacaoheliel <mercadoheliel@gmail.com>
2025-03-29 22:46:12 +0000

    criando supertrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,36 +1,25 @@
 #include <stdio.h>
 int main() {
     
-    int população=200.0000, numero de pontos turísticos=50;
-    float área=100.000,PIB=699.28;
-    char código,cidade [50];
-    char estado;"A";
-
-    prinft("Digite população:\n");
-    scanf("%2", & população);
+    int carta1;
+    int populacao=200.000;
+    char estado="A";
+    char codigo[50];
+    char cidade[20];
     
-    printf("Digite número de pontos turísticos:\n");
-    scanf("%d", & numero de ponto turísticos);
+    printf("carta1\n");
 
-    printf(" Digite área: \n");
-    scanf("%2f",área);
-
-    printf("Digite PIB: \n");
-    scanf("%2f",PIB);
-
-    printf("Digite seu código: \n");
-    scanf("%d",código);
-
-    printf("Digite nome da cidade: \n");
-    scanf("%s", cidade);
-
-    printf("Digite o estado:\n");
-    scanf("%s",estado);
-
+    printf("estado: A\n");
     
-    printf("estado:%s-código:%d-cidade:%s");
-    printf("população:%2-número de pontos turísticos:%d");
-    printf("área:%2f-PIB:%2f");
+    printf("populacao:200.000 \n");
+    
+    printf("codigo: \n");
+    scanf("%s",codigo);
+    
+    printf("Digite a cidade:\n");
+    scanf("%s", & cidade);
+
+    printf("codigo:%s-cidade:%s");
     
 
 
